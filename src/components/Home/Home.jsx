ca94c4194e01fff7985b2f08debf3c827b12d3ee
md-fahim18 <fahimzarif678@gmail.com>
2025-07-01 02:42:46 +0600

    hjj

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-            This line send zarif
-        </div>
-    );
+  return <div>Home This line send zarif fahim</div>;
 };
 
 export default Home;
