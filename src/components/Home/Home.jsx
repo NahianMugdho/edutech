ac230d237e2a3db6bfa7c8195b957b01c8493347
Abdulkaium007 <smakaium6957@gmail.com>
2025-07-02 00:29:41 +0600

    sssk

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,10 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            Home
 
-            This line send kaium
-
-            2nd push now
-
-
-            This line send Al Nahian Mugdho 
-
-            This line send zarif
-
-
-            This line send  Mugdho Nahian
-
-
-        </div>
-    );
   return (
     <div>
-      Home This line send kaium This line send Al Nahian Mugdho This line send
-      zarif This line send Mugdho Nahian this is new line
+        fghfjfjhhj
     </div>
   );
 };
