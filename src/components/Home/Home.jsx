626b742e1f8eeb9a4e408f443e1c46058e763305
md-fahim18 <fahimzarif678@gmail.com>
2025-07-02 00:48:57 +0600

    slider-implementedd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,6 @@ import MySlider from "../Slider/Slider";
 const Home = () => {
   return (
     <div>
-      Home This line send kaium This line send Al Nahian Mugdho This line send
-      zarif This line send Mugdho Nahian this is new line
       <MySlider></MySlider>
     </div>
   );
