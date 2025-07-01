b920271f05ee85062794c05bfc8514ed510d837f
Nahian Mugdho <nahianmugdho@gmail.com>
2025-07-02 00:36:49 +0600

    tes3

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,8 @@ const Home = () => {
   return (
     <div>
       Home This line send kaium This line send Al Nahian Mugdho This line send
-      zarif This line send Mugdho Nahian this is new line
+      zarif This line send Mugdho Nahian this is new line........
+      alll wil be in Homepage
       <MySlider></MySlider>
     </div>
   );
