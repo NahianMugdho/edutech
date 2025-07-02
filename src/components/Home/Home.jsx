093889f6a73a58ffabaa6ddec1001e84a1b6f059
md-fahim18 <fahimzarif678@gmail.com>
2025-07-02 22:54:35 +0600

    ser

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,6 @@ import MySlider from "../Slider/Slider";
 const Home = () => {
   return (
     <div>
-      Home This line send kaium This line send Al Nahian Mugdho This line send
-      zarif This line send Mugdho Nahian this is new line........ alll wil be in
-      Homepage
       <MySlider></MySlider>
     </div>
   );
