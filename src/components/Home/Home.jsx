e9ea10834ad4930defb87a1921f935c314ac99af
md-fahim18 <fahimzarif678@gmail.com>
2025-07-03 03:08:27 +0600

    course-card-updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MySlider from "../Slider/Slider";
+import Courses from "../Courses/Courses";
 
 const Home = () => {
   return (
     <div>
       <MySlider></MySlider>
+      <Courses></Courses>
     </div>
   );
 };
